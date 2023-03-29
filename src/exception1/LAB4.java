533792692f2ea1455fbaa453fa6ef2ab99aa13fa
nalinee <nalinee.nil@gmail.com>
2023-03-29 12:07:38 +0700

    exception1

---
+++ src/exception1/LAB4.java
@@ -0,0 +1,25 @@
+
+package exception1;
+
+
+public class LAB4 {
+    public static void main(String[] args) {
+        int[]list = new int[10];
+        try{
+            System.out.println("Statement1");
+            
+            System.out.println(list[10]);
+            System.out.println("Statement3");
+        }
+        catch (ArithmeticException e){
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            throw new StringIndexOutOfBoundsException();
+        }
+        
+        finally {
+            System.out.println("Statement 4"); 
+        }
+        System.out.println("Statement 5");
+    }
+}
